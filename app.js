c23f6300a6ab08c1bb063fc000dd8d2dae39ab82
Areez Visram <visraa7@mcmaster.ca>
2022-07-20 15:40:55 -0400

    Checking token status

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const contactMeRouter = require('./routes/contact-me');
 const app = express();
 
 // JWT middleware
-// app.use(jwtAuthenticationMiddleware);
+app.use(jwtAuthenticationMiddleware);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,11 +27,9 @@ app.use(cors());
 
 // Routes
 app.post('/jwt-login', jwtLogin);
-// app.use(routes.ABOUT_ROUTE, isAuthenticatedMiddleware, aboutRouter);
-app.use(routes.ABOUT_ROUTE, aboutRouter);
+app.use(routes.ABOUT_ROUTE, isAuthenticatedMiddleware, aboutRouter);
 app.use(routes.EXPERIENCE_ROUTE, isAuthenticatedMiddleware, experienceRouter);
 app.use(routes.PROJECTS_ROUTE, isAuthenticatedMiddleware, projectsRouter);
-// app.use(routes.PROJECTS_ROUTE, projectsRouter);
 app.use(routes.CONTACT_ME_ROUTE, isAuthenticatedMiddleware, contactMeRouter);
 
 module.exports = app;
