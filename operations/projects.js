f3f4efe4528777ee10dcb1606d73b71d0a731c53
Areez Visram <visraa7@mcmaster.ca>
2022-07-21 15:34:15 -0400

    Doing cloudinary upload before project upload

--- operations/projects.js
+++ operations/projects.js
@@ -28,20 +28,22 @@ const postProject = async (document) => {
 
     cache.del(`__express__${partition}${routes.PROJECTS_ROUTE}${routes.PROJECTS_GET}`);
 
-    const imagePath = document.imagePath;
-    delete document.imagePath;
+    // const imagePath = document.imagePath;
+    // delete document.imagePath;
 
+    // let response;
+    // await cloudinaryUpload(imagePath).then((res) => {
+    //     const image_url = res.url;
+    //     document = Object.assign({ "image_url": image_url }, document);
+    // }).then(async () => {
+
+    // }).catch((err) => {
+    //     console.log(err)
+    // })
     let response;
-    await cloudinaryUpload(imagePath).then((res) => {
-        const image_url = res.url;
-        document = Object.assign({ "image_url": image_url }, document);
-    }).then(async () => {
-        await postDocumentPartitioned(service, document, projectsDatabase).then((res) => {
-            response = res.result;
-        })
-    }).catch((err) => {
-        console.log(err)
-    })
+    await postDocumentPartitioned(service, document, projectsDatabase).then((res) => {
+        response = res.result;
+    });
 
 
     // await postDocumentPartitioned(service, document, projectsDatabase).then((res) => {
