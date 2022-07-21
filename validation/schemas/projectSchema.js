756c34546aaaef4e40a16579985c6fdbf440826f
Areez Visram <visraa7@mcmaster.ca>
2022-07-21 15:36:06 -0400

    Update joi validation schema to use image_url instead of imagePath

--- validation/schemas/projectSchema.js
+++ validation/schemas/projectSchema.js
@@ -2,14 +2,14 @@
 const joi = require('joi');
 
 const projectSchema = joi.object({
-    name: joi.string().required(),    
+    name: joi.string().required(),
     description: joi.string().required(),
     languages: joi.array().items(joi.string()).required(),
     github: joi.string().required(),
     external: joi.string().required(),
     partition: joi.string().required().valid('non-featured', 'featured'),
     index: joi.number().integer().required().strict(),
-    imagePath: joi.string().when('partition', { is: 'featured', otherwise: joi.forbidden() })
+    image_url: joi.string().when('partition', { is: 'featured', otherwise: joi.forbidden() })
 });
 
 module.exports = {
