3ac51c4b093ea218d6a674f2229c9767706cb036
Areez Visram <visraa7@mcmaster.ca>
2022-07-22 09:53:59 -0400

    External link is not required

--- validation/schemas/projectSchema.js
+++ validation/schemas/projectSchema.js
@@ -6,7 +6,6 @@ const projectSchema = joi.object({
     description: joi.string().required(),
     languages: joi.array().items(joi.string()).required(),
     github: joi.string().required(),
-    external: joi.string().required(),
     partition: joi.string().required().valid('non-featured', 'featured'),
     index: joi.number().integer().required().strict(),
     image_url: joi.string().when('partition', { is: 'featured', otherwise: joi.forbidden() })
